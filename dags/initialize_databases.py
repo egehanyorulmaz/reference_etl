84bf22ba247e63ca939646a18906f4b7c22d3b26
egehanyorulmaz <egehanyorulmaz@sabanciuniv.edu>
2022-03-19 14:53:39 +0300

    changes company keyword to stock

--- dags/initialize_databases.py
+++ dags/initialize_databases.py
@@ -5,23 +5,23 @@ import os
 CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 database = Mysql(host='localhost', port='3306', db_name='mysql_db', user_name='root', password='root_mysql')
 
-database.drop_table(table_schema='company', table_name='company_symbols')
-database.drop_table(table_schema='company', table_name='company_values')
-database.drop_schema(table_schema='company')
+database.drop_table(table_schema='stock', table_name='stock_symbols')
+database.drop_table(table_schema='stock', table_name='stock_values')
+database.drop_schema(table_schema='stock')
 
-database.create_schema('company')
+database.create_schema('stock')
 
-# Reads the data, creates the table and inserts values to company_symbols
-company_symbols_df = pd.read_csv(CUR_DIR + "/tutorial_data/Company.csv")
-database.create_table(table_schema='company', table_name='company_symbols',
+# Reads the data, creates the table and inserts values to stock_symbols
+stock_symbols_df = pd.read_csv(CUR_DIR + "/tutorial_data/Company.csv")
+database.create_table(table_schema='stock', table_name='stock_symbols',
                       columns={'ticker_symbol': 'varchar(20)',
-                               'company_name': 'varchar(20)'})
-database.insert_values(data=company_symbols_df, table_schema='company', table_name='company_symbols',
-                       columns='ticker_symbol, company_name')
+                               'stock_name': 'varchar(20)'})
+database.insert_values(data=stock_symbols_df, table_schema='stock', table_name='stock_symbols',
+                       columns='ticker_symbol, stock_name')
 
-# Reads the data, creates the table and inserts values to company_values
-company_values_df = pd.read_csv(CUR_DIR + "/tutorial_data/CompanyValues.csv")
-database.create_table(table_schema='company', table_name='company_values',
+# Reads the data, creates the table and inserts values to stock_values
+stock_values_df = pd.read_csv(CUR_DIR + "/tutorial_data/CompanyValues.csv")
+database.create_table(table_schema='stock', table_name='stock_values',
                       columns={'ticker_symbol': 'varchar(20)',
                                'day_date': 'timestamp',
                                'close_value': 'float',
@@ -29,7 +29,7 @@ database.create_table(table_schema='company', table_name='company_values',
                                'open_value': 'float',
                                'high_value': 'float',
                                'low_value': 'float'})
-database.insert_values(data=company_values_df, table_schema='company', table_name='company_values',
+database.insert_values(data=stock_values_df, table_schema='stock', table_name='stock_values',
                        columns='ticker_symbol, day_date, close_value, volume, open_value, high_value, low_value')
 
 print("Data is ready!")
--- dags/initialize_reference_table.py
+++ dags/initialize_reference_table.py
@@ -7,11 +7,11 @@ CUR_DIR = os.path.abspath(os.path.dirname(__file__))
 database = Postgresql(host='localhost', port='5433', db_name='postgres_db', user_name='postgres', password='postgres')
 
 # initialize destination tables for data ingestion
-database.create_schema('company')
-database.create_table(table_schema='company', table_name='company_symbols',
+database.create_schema('stock')
+database.create_table(table_schema='stock', table_name='stock_symbols',
                       columns={'ticker_symbol': 'varchar',
-                               'company_name': 'varchar'})
-database.create_table(table_schema='company', table_name='company_values',
+                               'stock_name': 'varchar'})
+database.create_table(table_schema='stock', table_name='stock_values',
                       columns={'ticker_symbol': 'varchar',
                                'day_date': 'timestamp',
                                'close_value': 'numeric',
@@ -37,35 +37,35 @@ database.create_table(table_schema='etl_manager', table_name='database_flow_refe
 
 database.truncate_table(table_schema='etl_manager', table_name='database_flow_reference_table')
 
-company_symbol_dict = {'insert_date': str(datetime.now()), 'source_connection': 'mysql',
-                       'source_schema': 'company',
-                       'source_table': 'company_symbols', 'key_fields': 'ticker_symbol, company_name',
+stock_symbol_dict = {'insert_date': str(datetime.now()), 'source_connection': 'mysql',
+                       'source_schema': 'stock',
+                       'source_table': 'stock_symbols', 'key_fields': 'ticker_symbol, stock_name',
                        'extraction_method': 'jdbc',
                        'extraction_type': 'full', 'destination_connection': 'postgresql',
-                       'destination_schema': 'company',
-                       'destination_table': 'company_symbols', 'target_fields': 'ticker_symbol, company_name'}
+                       'destination_schema': 'stock',
+                       'destination_table': 'stock_symbols', 'target_fields': 'ticker_symbol, stock_name'}
 
-company_values_dict = {'insert_date': str(datetime.now()), 'source_connection': 'mysql',
-                       'source_schema': 'company',
-                       'source_table': 'company_values',
+stock_values_dict = {'insert_date': str(datetime.now()), 'source_connection': 'mysql',
+                       'source_schema': 'stock',
+                       'source_table': 'stock_values',
                        'key_fields': 'ticker_symbol, day_date, close_value, volume, '
                                      'open_value, high_value, low_value',
                        'extraction_method': 'jdbc',
                        'extraction_type': 'full', 'destination_connection': 'postgresql',
-                       'destination_schema': 'company',
-                       'destination_table': 'company_values',
+                       'destination_schema': 'stock',
+                       'destination_table': 'stock_values',
                        'target_fields': 'ticker_symbol, day_date, close_value, volume, '
                                         'open_value, high_value, low_value'}
 
-company_symbol_dict = {k: [v, ] for k, v in company_symbol_dict.items()}
-company_values_dict = {k: [v, ] for k, v in company_values_dict.items()}
+stock_symbol_dict = {k: [v, ] for k, v in stock_symbol_dict.items()}
+stock_values_dict = {k: [v, ] for k, v in stock_values_dict.items()}
 
-company_symbol_df = pd.DataFrame(company_symbol_dict)
-company_values_df = pd.DataFrame(company_values_dict)
+stock_symbol_df = pd.DataFrame(stock_symbol_dict)
+stock_values_df = pd.DataFrame(stock_values_dict)
 
-database.insert_values(data=company_symbol_df, table_schema='etl_manager', table_name='database_flow_reference_table',
-                       columns=', '.join(company_symbol_df.columns.tolist()))
-database.insert_values(data=company_values_df, table_schema='etl_manager', table_name='database_flow_reference_table',
-                       columns=', '.join(company_values_df.columns.tolist()))
+database.insert_values(data=stock_symbol_df, table_schema='etl_manager', table_name='database_flow_reference_table',
+                       columns=', '.join(stock_symbol_df.columns.tolist()))
+database.insert_values(data=stock_values_df, table_schema='etl_manager', table_name='database_flow_reference_table',
+                       columns=', '.join(stock_values_df.columns.tolist()))
 
 database.close_connection()
